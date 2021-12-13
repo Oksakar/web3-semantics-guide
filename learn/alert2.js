c6420b09b38caf6cd87e2f87bd3a2fdaf43ea44f
Oksana Karpova <ksushakarpova83@gmail.com>
2021-12-13 20:12:39 +0200

    added variables

--- learn/alert2.js
+++ learn/alert2.js
@@ -1,6 +1,10 @@
-const person2 = {
-  firstname: "John",
-  lastname: "Doe",
-};
+let name = "Ilya";
 
-alert(person2["lastname"]);
+// выражение - число 1
+alert(`hello ${1}`); // hello 1
+
+// выражение - строка "name"
+alert(`hello ${"name"}`); // hello name
+
+// выражение - переменная, вставим её в строку
+alert(`hello ${name}`); // hello Ilya
