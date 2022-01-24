87489236bdd1a53b5f90d5f5bd6e29278688f32b
Oksana Karpova <ksushakarpova83@gmail.com>
2022-01-24 18:52:47 +0200

    application of hoisting to variable declarations and to function declarations

--- learn/beginner-html-site-styled-gh-pages/scripts/main.js
+++ learn/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,17 +1,34 @@
-var myImage = document.querySelector("img");
-
-myImage.onclick = function () {
-  var mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/firefox2.png");
-  } else {
-    myImage.setAttribute("src", "images/firefox-icon.png");
-  }
-};
-var myButton = document.querySelector("button");
-var myHeading = document.querySelector("h1");
-function setUserName() {
-  var myName = prompt("Please enter your name.");
-  localStorage.setItem("name", myName);
-  myHeading.textContent = "Mozilla is cool, " + myName;
+function addNumbers(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 }
+// Sample usage
+addNumbers(1, 2); // 3
+addNumbers(3, 2); // 5
+
+function multiply(a, b) {
+  return a * b;
+}
+// Sample usage
+multiply(1, 2); // 2
+multiply(2, 2); // 4
+
+function double(x) {
+  return x * 2;
+}
+// Sample usage
+double(2); // 4
+double(5); // 10
+
+function nextDecade(year) {
+  return year + 10;
+}
+// Sample usage
+nextDecade(2020); // 2030
+nextDecade(2000); // 2010
+
+function nextYear(year) {
+  return year + 1;
+}
+// Sample usage
+nextYear(2000); // 2001
+nextYear(2020); // 2021
