7c4f75fadf1440d31e0d15e3e03c738535c4da5c
Oksana Karpova <ksushakarpova83@gmail.com>
2022-01-27 14:30:09 +0200

    using ternary operators in code

--- learn/beginner-html-site-styled-gh-pages/scripts/main.js
+++ learn/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,34 +1,11 @@
-function addNumbers(firstNumber, secondNumber) {
-  return firstNumber + secondNumber;
-}
-// Sample usage
-addNumbers(1, 2); // 3
-addNumbers(3, 2); // 5
+let stopLight = "yellow";
 
-function multiply(a, b) {
-  return a * b;
+if (stopLight === "red") {
+  console.log("Stop!");
+} else if (stopLight === "yellow") {
+  console.log("Slow down.");
+} else if (stopLight === "green") {
+  console.log("Go!");
+} else {
+  console.log("Caution, unknown!");
 }
-// Sample usage
-multiply(1, 2); // 2
-multiply(2, 2); // 4
-
-function double(x) {
-  return x * 2;
-}
-// Sample usage
-double(2); // 4
-double(5); // 10
-
-function nextDecade(year) {
-  return year + 10;
-}
-// Sample usage
-nextDecade(2020); // 2030
-nextDecade(2000); // 2010
-
-function nextYear(year) {
-  return year + 1;
-}
-// Sample usage
-nextYear(2000); // 2001
-nextYear(2020); // 2021
