8cad0b81fa7bb4e28b2b49ebfaf6b7528eed91b0
Oksana Karpova <ksushakarpova83@gmail.com>
2022-01-28 20:16:01 +0200

    used a function expression

--- learn/beginner-html-site-styled-gh-pages/scripts/main.js
+++ learn/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,11 +1,7 @@
-let stopLight = "yellow";
-
-if (stopLight === "red") {
-  console.log("Stop!");
-} else if (stopLight === "yellow") {
-  console.log("Slow down.");
-} else if (stopLight === "green") {
-  console.log("Go!");
-} else {
-  console.log("Caution, unknown!");
+function getReminder() {
+  console.log("Water the plants.");
 }
+function greetInSpanish() {
+  console.log("Buenas Tardes.");
+}
+greetInSpanish();
