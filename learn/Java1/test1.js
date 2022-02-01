0350c065741353c5299c5d71b6fb7df03a048baf
Oksana Karpova <ksushakarpova83@gmail.com>
2022-02-01 20:05:39 +0200

    adding multiple array methods to edit the program

--- learn/Java1/test1.js
+++ learn/Java1/test1.js
@@ -18,3 +18,25 @@ const name2 = "Nala";
 
 logEvent(name2, event2);
 logEvent("masha", "vasya");
+
+const flowers = ["peony", "daffodil", "marigold"];
+
+function addFlower(arr) {
+  arr.push("lily");
+}
+
+addFlower(flowers);
+
+console.log(flowers); // Output: ['peony', 'daffodil', 'marigold', 'lily']
+const chores = [
+  "wash dishes",
+  "do laundry",
+  "take out trash",
+  "cook dinner",
+  "mop floor",
+];
+
+chores.pop();
+const foo = chores.pop();
+console.log(chores);
+console.log(foo);
