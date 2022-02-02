30e40907b314aada03e05b50577e8bde2b8605a1
Oksana Karpova <ksushakarpova83@gmail.com>
2022-02-02 19:54:55 +0200

    using nested loop in the program

--- learn/Java1/test1.js
+++ learn/Java1/test1.js
@@ -1,42 +1,6 @@
-function getRandEvent() {
-  const random = Math.floor(Math.random() * 3);
-  if (random === 0) {
-    return "Marathon";
-  } else if (random === 1) {
-    return "Triathlon";
-  } else if (random === 2) {
-    return "Pentathlon";
+for (let i = 0; i < 5; i++) {
+  console.log(`The current value of i is: ${i}`);
+  for (let j = 0; j < 5; j++) {
+    console.log(`j: ${j}`);
   }
 }
-
-function logEvent(stringForName, stringForResult) {
-  console.log(`${stringForName}'s event is: ${stringForResult}`);
-}
-
-const event2 = getRandEvent();
-const name2 = "Nala";
-
-logEvent(name2, event2);
-logEvent("masha", "vasya");
-
-const flowers = ["peony", "daffodil", "marigold"];
-
-function addFlower(arr) {
-  arr.push("lily");
-}
-
-addFlower(flowers);
-
-console.log(flowers); // Output: ['peony', 'daffodil', 'marigold', 'lily']
-const chores = [
-  "wash dishes",
-  "do laundry",
-  "take out trash",
-  "cook dinner",
-  "mop floor",
-];
-
-chores.pop();
-const foo = chores.pop();
-console.log(chores);
-console.log(foo);
