36057a974162dc10160da3f6104c9fd1318110de
Oksana Karpova <ksushakarpova83@gmail.com>
2022-02-03 19:50:57 +0200

    used the trim() function to remove the whitespaces from console.log

--- learn/Java1/test1.js
+++ learn/Java1/test1.js
@@ -1,6 +1,23 @@
-for (let i = 0; i < 5; i++) {
+/*for (let i = 0; i < 3; i++) {
   console.log(`The current value of i is: ${i}`);
-  for (let j = 0; j < 5; j++) {
+  for (let j = 0; j < 3; j++) {
     console.log(`j: ${j}`);
   }
+  console.log("--------------");
+}*/
+let input = "Free Willy the while";
+const vowels = ["a", "e", "i", "o", "u"];
+let resultArray = [];
+for (let i = 0; i < input.length; i++) {
+  //console.log('i is '+ i);
+  for (let j = 0; j < vowels.length; j++) {
+    //console.log('j is '+ j)
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+      if (input[i] === "e" || input[i] === "u") {
+        resultArray.push(input[i]);
+      }
+    }
+  }
 }
+console.log(resultArray);
