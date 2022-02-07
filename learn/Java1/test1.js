a00c6f9624e63994e43cf4a4472d7e80f5e52a79
Oksana Karpova <ksushakarpova83@gmail.com>
2022-02-07 13:34:53 +0200

    used in the program nested objects

--- learn/Java1/test1.js
+++ learn/Java1/test1.js
@@ -1,23 +1,79 @@
-/*for (let i = 0; i < 3; i++) {
-  console.log(`The current value of i is: ${i}`);
-  for (let j = 0; j < 3; j++) {
-    console.log(`j: ${j}`);
+/*const animal = "cat";
+
+for (let i = 0; i < animal.length; i++) {
+  console.log(animal[i]);
+  for (let j = 1; j < 4; j++) {
+    console.log(j);
   }
-  console.log("--------------");
 }*/
-let input = "Free Willy the while";
-const vowels = ["a", "e", "i", "o", "u"];
-let resultArray = [];
-for (let i = 0; i < input.length; i++) {
-  //console.log('i is '+ i);
-  for (let j = 0; j < vowels.length; j++) {
-    //console.log('j is '+ j)
-    if (input[i] === vowels[j]) {
-      resultArray.push(input[i]);
-      if (input[i] === "e" || input[i] === "u") {
-        resultArray.push(input[i]);
-      }
-    }
-  }
+/*for (let i = 0; i < 10; i += 3) {
+  console.log(i);
+}*/
+/*const groceryList = [
+  "orange juice",
+  "bananas",
+  "coffee beans",
+  "brown rice",
+  "pasta",
+  "coconut oil",
+  "plantains",
+];
+
+for (let item of groceryList) {
+  console.log("I love " + item);
 }
-console.log(resultArray);
+
+function muFunction(item, index, array) {
+  console.log(
+    `Элемент: ${item} 
+        Индекс: ${index}
+        Массив: ${array}`
+  );
+}
+
+let arr = [1, 2, 3, 4, 5];*/
+let spaceship = {
+  passengers: [
+    {
+      pass1: {
+        name: "Oksana",
+        age: 39,
+      },
+      pass2: {
+        name: "Dima",
+        age: 42,
+      },
+    },
+  ],
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032,
+  },
+  crew: {
+    captain: {
+      name: "Sandra",
+      degree: "Computer Engineering",
+      encourageTeam() {
+        console.log("We got this!");
+      },
+      "favorite foods": ["cookies", "cakes", "candy", "spinach"],
+    },
+  },
+  engine: {
+    model: "Nimbus2000",
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD",
+    },
+    "back-up": {
+      battery: "Lithium",
+      terabytes: 50,
+    },
+  },
+};
+let capFave = spaceship.crew.captain["favorite foods"][0];
+let firstPassenger = spaceship.passengers[0];
+console.log(Object.keys(spaceship));
